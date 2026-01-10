640d34c50844dfceb44452b84ac4a382b77da08e
王帅 <13177217912ws@gmail.com>
2026-01-10 13:39:18 +0800

    feat: add test route

---
+++ src/app/rewrite-test/page.tsx
@@ -0,0 +1,10 @@
+
+export const dynamic = 'force-dynamic';
+
+export default function RewriteTestPage() {
+    return (
+        <div className="p-10 bg-red-500 text-white text-4xl font-bold">
+            HELLO WORLD V3 - NEW ROUTE TEST
+        </div>
+    );
+}
